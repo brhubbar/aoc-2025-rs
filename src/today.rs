3a2741929455c3e481210005e1e4d6bf2dc12ce0
Benjamin Hubbard <brhubbar@mtu.edu>
2025-12-05 12:07:29 -0600

    Still trying and failing on day 2

--- src/today.rs
+++ src/today.rs
@@ -4,4 +4,4 @@ pub mod day3;
 pub mod day4;
 pub mod day5;
 
-pub use day5::*;
+pub use day2::*;
--- src/today/day2.rs
+++ src/today/day2.rs
@@ -33,9 +33,9 @@ use log::{debug, trace, warn};
 /// the numbers, adjust the halves by one inward to the range.
 ///
 ///
-pub fn part1(contents: &str) -> u64 {
-    let spans = contents.trim().split(',');
-    let mut sum: u64 = 0;
+pub fn part1(contents: &str) -> u128 {
+    let spans: std::str::Split<'_, char> = contents.trim().split(',');
+    let mut ret = 0;
     for span in spans {
         let mut ends = span.split('-');
         let low = ends
@@ -44,27 +44,16 @@ pub fn part1(contents: &str) -> u64 {
         let high = ends
             .next()
             .expect("Range should take the shape {low}-{high}");
-        trace!(target: "part1", "{low}, {high}");
-        let (low, high) = filter_range(low, high);
-        if low == "0" && high == "0" {
-            warn!(target: "part1", "!! SKIPPING {low}-{high}");
-            continue;
-        }
+        trace!(target: "part2", "{low}, {high}");
 
-        let low = split_num(low, true);
-        let high = split_num(high, false);
-        trace!(target: "part1", "{low}-{high}");
+        let lo: u128 = low.parse().expect("Should be a number.");
+        let hi: u128 = high.parse().expect("Should be a number.");
 
-        for x in low..high {
-            trace!("{x},");
-            let x = x.to_string();
-            let mut id = String::new();
-            id.push_str(&x);
-            id.push_str(&x);
-            sum += id.parse::<u64>().expect("id should be numeric.")
+        for length in low.len()..high.len() + 1 {
+            ret += sum_rep(lo, hi, length as u128, 2)
         }
     }
-    sum
+    ret
 }
 
 /// So do this again, but the sequence repeats two or more times - therefore, no longer working with
@@ -88,23 +77,9 @@ pub fn part1(contents: &str) -> u64 {
 /// - 450 450
 ///
 /// So I think I can re-use my string manipulation, I just have to push more times.
-pub fn part2(contents: &str) -> u64 {
-    let valid_component_lengths: Vec<Vec<u32>> = vec![
-        vec![],        // 0
-        vec![],        // 1
-        vec![1],       // 2
-        vec![1],       // 3
-        vec![1, 2],    // 4
-        vec![1],       // 5
-        vec![1, 3],    // 6
-        vec![1],       // 7
-        vec![1, 2, 4], // 8
-        vec![1, 3],    // 9
-        vec![1, 2, 5], // 10
-    ];
-
-    let spans = contents.trim().split(',');
-    let mut sum: u64 = 0;
+pub fn part2(contents: &str) -> u128 {
+    let spans: std::str::Split<'_, char> = contents.trim().split(',');
+    let mut ret = 0;
     for span in spans {
         let mut ends = span.split('-');
         let low = ends
@@ -114,81 +89,113 @@ pub fn part2(contents: &str) -> u64 {
             .next()
             .expect("Range should take the shape {low}-{high}");
         trace!(target: "part2", "{low}, {high}");
-        let (low, high) = filter_range(low, high);
-        if low == "0" && high == "0" {
-            warn!(target: "part2", "!! SKIPPING {low}-{high}");
-            continue;
-        }
 
-        let len = high.len();
-        for valid_component_length in &valid_component_lengths[len] {
-            let substring_length: usize = (valid_component_length * 2)
-                .try_into()
-                .expect("Should fit in a usize.");
-            let low = split_num(&truncate(low, substring_length), true);
-            let high = split_num(&truncate(high, substring_length), false);
-            trace!(target: "part2", "Parts: {low}-{high}");
+        let lo: u128 = low.parse().expect("Should be a number.");
+        let hi: u128 = high.parse().expect("Should be a number.");
 
-            for x in low..high {
-                trace!(target: "part2", "Part: {x}");
-                let x = x.to_string();
-                let mut id = String::new();
-                for _ in 0..(len / (substring_length / 2)) {
-                    id.push_str(&x);
+        for length in low.len()..high.len() + 1 {
+            let mut rep_counts = vec![0; (length / 2 + 1) as usize];
+
+            for l in 1..(length as u128 / 2 + 1) {
+                rep_counts[l as usize] = sum_rep(lo, hi, length as u128, l)
+            }
+            for l in 1..(length / 2 + 1) {
+                if !length.is_multiple_of(l) {
+                    continue;
                 }
-                trace!(target: "part2", "Id: {id}");
-                sum += id.parse::<u64>().expect("id should be numeric.")
+
+                ret += rep_counts[l as usize];
+
+                for x in 1..l {
+                    if l.is_multiple_of(x) {
+                        ret -= rep_counts[x as usize];
+                    }
+                }
             }
         }
     }
-    sum
+    ret
+}
+/// Trying a closed formula solution from reddit.
+/// https://www.reddit.com/r/adventofcode/comments/1pcbgai/2025_day_2_day_2_should_be_easy_right_closed/
+///
+/// p(r,j) = ( 10^((j+1)r) - 1 ) / ( 10^(j+1) - 1 )
+/// r = n_repeats
+/// j = n_digits_to_repeat
+fn repeater(n_repeats: u32, n_digits_to_repeat: u32) -> u128 {
+    (10u128.pow((n_digits_to_repeat) * n_repeats) - 1) / (10u128.pow(n_digits_to_repeat) - 1)
 }
 
-/// Check if the range can possibly have a repeated number in it (there is a number with an even
-/// number of digits within the range).
-fn filter_range<'a>(low: &'a str, high: &'a str) -> (&'a str, &'a str) {
-    let n_digits_low = low.len();
-    let n_digits_high = high.len();
+fn first_exceeding_pattern(
+    number_to_exceed: u128,
+    n_repeats: u32,
+    n_digits_to_repeat: u32,
+) -> u128 {
+    std::cmp::min(
+        number_to_exceed.div_ceil(repeater(n_repeats, n_digits_to_repeat)),
+        10u128.pow(n_digits_to_repeat),
+    )
+}
 
-    if n_digits_low % 2 == 1 && n_digits_high % 2 == 1 {
-        if n_digits_low == n_digits_high {
-            return ("0", "0");
-        }
-        // We never hit this, so I don't need to deal with this logic. Whoop, whoop.
-        panic!("NOT THE SAME ODD LENGTH")
+// Blah! Mobius? Huh?
+
+/// Copying a different solution from jimm89:
+/// https://github.com/jimm89/AdventOfCode2025/blob/main/Day%202/Day%202.ipynb
+fn sum_rep(lo: u128, hi: u128, length: u128, rep: u128) -> u128 {
+    if !length.is_multiple_of(rep) {
+        return 0;
     }
-    (low, high)
-}
 
-/// Return the number as needed for computing the repeated ids.
-fn split_num(num: &str, is_low: bool) -> u64 {
-    let len: u32 = num.len().try_into().expect("Length should fit into a u32.");
-    let num = if len % 2 == 1 {
-        if is_low {
-            debug!("! ROUNDING UP");
-            (10_u64.pow(len)).to_string()
-        } else {
-            debug!("! ROUNDING DOWN");
-            ((10_u64.pow(len - 1)) - 1).to_string()
-        }
+    let rep: u128 = length / rep;
+
+    let lo_rep = 10u128.pow((length / rep - 1) as u32);
+    let hi_rep = 10 * lo_rep - 1;
+
+    let mut min;
+    if length > (lo.to_string().len() as u128) {
+        min = lo_rep;
     } else {
-        String::from(num)
-    };
+        min = lo;
+        min /= (lo_rep * 10).pow((rep - 1) as u32);
+        let mut test = min;
+        for _ in 0..rep {
+            test = test * (10 * lo_rep) + min
+        }
+        if test < lo {
+            min += 1
+        }
+    }
+    let min = min;
 
-    let half = num.len() / 2;
+    let mut max;
+    if length < (hi.to_string().len() as u128) {
+        max = hi_rep;
+    } else {
+        max = hi;
+        max /= (lo_rep * 10).pow((rep - 1) as u32);
+        let mut test = max;
+        for _ in 0..rep - 1 {
+            test = test * (10 * lo_rep) + max;
+        }
+        if (test) > hi {
+            max -= 1;
+        }
+    }
+    let max = max;
 
-    let mut first_half: u64 = num[..half].parse().expect("Num should parse to a u64.");
-    let second_half: u64 = num[half..].parse().expect("Num should parse to a u64.");
+    let mut ret = sum_range(min, max);
+    let adder = ret;
+    for _ in 0..(rep - 1) {
+        ret = ret * (10 * lo_rep) + adder;
+    }
 
-    debug!("{num} ({len}) -> {first_half} {second_half}");
-
-    if is_low && first_half < second_half || !is_low && first_half <= second_half {
-        first_half += 1;
-    };
-
-    first_half
+    ret
 }
 
-fn truncate(string: &str, length: usize) -> String {
-    String::from_iter(string.chars().take(length))
+fn sum_first_n(n: u128) -> u128 {
+    n * (n + 1) / 2
+}
+
+fn sum_range(min: u128, max: u128) -> u128 {
+    sum_first_n(max) - sum_first_n(min - 1)
 }
