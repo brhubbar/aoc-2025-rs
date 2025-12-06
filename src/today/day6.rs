bb3f3ae662db8b6233d59a8f7095ba56b214adfd
Benjamin Hubbard <brhubbar@mtu.edu>
2025-12-06 16:32:28 -0600

    Implement day 6 part 1

--- src/today/day6.rs
+++ src/today/day6.rs
@@ -1,5 +1,40 @@
-pub fn part1(contents: &str) -> u32 {
-    1
+use log::trace;
+use regex::Regex;
+use std::collections::HashMap;
+
+pub fn part1(contents: &str) -> u64 {
+    let mut ops_map: HashMap<&str, fn(u64, u64) -> u64> = HashMap::new();
+    ops_map.insert("*", u64::saturating_mul);
+    ops_map.insert("+", u64::saturating_add);
+
+    let search = Regex::new(r"\s+").expect("Should be a valid regex");
+
+    let mut iterator = contents.trim().split('\n').rev();
+
+    let operations = iterator.next().expect("Should have a line.");
+    let operations: Vec<&str> = search.split(operations).collect();
+    trace!("Ops: {operations:?}");
+
+    let mut columns: Vec<u64> = search
+        .split(iterator.next().expect("Should have a line").trim())
+        .map(|x| x.parse::<u64>().expect("Should be a number"))
+        .collect();
+    trace!("Cols: {columns:?}");
+
+    for line in iterator {
+        trace!("Next: {line}");
+        let rhss = search
+            .split(line.trim())
+            .map(|x| x.parse::<u64>().expect("Should be a number"));
+
+        for (idx, rhs) in rhss.enumerate() {
+            let operation = ops_map.get(&operations[idx]).expect("Should be found.");
+            columns[idx] = operation(columns[idx], rhs);
+        }
+        trace!("Cols: {columns:?}");
+    }
+
+    columns.into_iter().sum()
 }
 pub fn part2(contents: &str) -> u32 {
     1
